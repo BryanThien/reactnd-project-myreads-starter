e42c32200046712b63e0795571c8bba5ee77a75f
Bryan Thien <eonearthproductions@gmail.com>
2018-12-25 23:48:32 -0600

    Adds books props to mainPage component

--- src/App.js
+++ src/App.js
@@ -16,9 +16,12 @@ class BooksApp extends React.Component {
 }
 
   render() {
+    
     return (
       <div className="app">
-        <MainPage/>
+        <MainPage
+          books={this.state.books}
+        />
       </div>
     )
   }
