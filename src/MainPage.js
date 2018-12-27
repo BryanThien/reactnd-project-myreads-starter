d525c6934a1816cb8ec6599e2bdbd04b324afc2f
Bryan Thien <eonearthproductions@gmail.com>
2018-12-26 21:00:27 -0600

    non-interactive components UI done

--- src/MainPage.js
+++ src/MainPage.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
+import Book from './Book';
 
 class MainPage extends Component {
   render() {
+    console.log(this.props.books);
     return (
       <div className="list-books">
         <div className="list-books-title">
@@ -13,7 +15,16 @@ class MainPage extends Component {
               <h2 className="bookshelf-title">Currently Reading</h2>
               <div className="bookshelf-books">
                 <ol className="books-grid">
-                  <li></li>
+                {
+                  this.props.books.filter(book => book.shelf === 'currentlyReading')
+                  .map(book => (
+                  <li key={book.id} >
+                    <Book 
+                      book={book}
+                    />
+                  </li>
+                  ))
+                }
                 </ol>
               </div>
             </div>
@@ -21,7 +32,15 @@ class MainPage extends Component {
               <h2 className="bookshelf-title">Want to Read</h2>
               <div className="bookshelf-books">
                 <ol className="books-grid">
-                  <li></li>
+                {
+                  this.props.books.filter(book => book.shelf === 'wantToRead')
+                  .map(book => (
+                  <li key={book.id} >
+                    <Book 
+                      book={book}/>
+                  </li>
+                  ))
+                }
                 </ol>
               </div>
             </div>
@@ -29,7 +48,16 @@ class MainPage extends Component {
               <h2 className="bookshelf-title">Read</h2>
               <div className="bookshelf-books">
                 <ol className="books-grid">
-                  <li></li>
+                {
+                  this.props.books.filter(book => book.shelf === 'read')
+                  .map(book => (
+                  <li key={book.id} >
+                    <Book 
+                      book={book}
+                    />
+                  </li>
+                  ))
+                }
                 </ol>
               </div>
             </div>
