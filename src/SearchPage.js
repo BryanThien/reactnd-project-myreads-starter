db75b94b728a1f9114bf5ee5d79af41c592c2a4b
Bryan Thien <eonearthproductions@gmail.com>
2018-12-29 20:55:35 -0600

    SearchPage component completed

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,6 +1,34 @@
 import React, { Component } from "react";
-
+import * as BooksAPI from './BooksAPI'
+import Book from './Book'
 class SearchPage extends Component {
+  state = {
+    query: '',
+    searchedBooks: []
+  }
+
+  updateQuery = (query) => {
+    this.setState({
+      query: query
+    })
+    this.updateSearchedBooks(query);
+  }
+
+  updateSearchedBooks = (query) => {
+    if (query) {
+      BooksAPI.search(query).then((searchedBooks) => {
+        if (searchedBooks.error) {
+          this.setState({ searchedBooks: [] });
+        } else {
+          this.setState({ searchedBooks: searchedBooks })
+        }       
+      })
+    } else {
+      this.setState({ searchedBooks: [] })
+    }
+    
+  }
+
   render() {
     return (
       <div className="search-books">
@@ -20,13 +48,29 @@ class SearchPage extends Component {
                     However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                     you don't find a specific author or title. Every search is limited by search terms.
                     */}
-            <input type="text" placeholder="Search by title or author" />
+            <input 
+              type="text" 
+              placeholder="Search by title or author" 
+              value={this.state.query}
+              onChange={(event) => this.updateQuery(event.target.value) }
+            />
+
           </div>
         </div>
         
         <div className="search-books-results">
 
-          <ol className="books-grid" />
+          <ol className="books-grid" >
+                  {
+                    this.state.searchedBooks.map(searchedBook => (
+                      <li key={searchedBook.id}>
+                        <Book
+                          book={searchedBook}
+                        />
+                      </li>
+                    ))
+                  } {/*working on making the search field at 1:54:28*/}
+          </ol>
 
         </div>
       </div>
